27aa320e90be252a17b07e6bab04a6a0d36bbeaa
fordster78 <jack-ford@outlook.com>
2018-01-14 19:14:51 +0000

    Initial Commit
    Working version of the Hardware Library. Version 1

---
+++ TestApp/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HardwareLibrary;
+
+namespace TestApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = SystemProperties.ComputerName();
+            label2.Text = SystemProperties.ComputerMake();
+            label3.Text = SystemProperties.ComputerModel();
+            label4.Text = SystemProperties.SerialNumber();
+        }
+    }
+}
